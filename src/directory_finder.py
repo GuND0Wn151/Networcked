160602aea4571c6c549bb249ae1af9e02a8b2d25
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-27 12:25:44 +0530

    Create directory_finder.py

---
+++ src/directory_finder.py
@@ -0,0 +1,21 @@
+import os
+import time
+import requests
+
+RED='\033[0;31m'
+NC='\033[0m'
+green='\033[0;32m'
+cyan='\033[0;36m'
+orange='\033[0;33m'
+purple='\033[0;35m'
+
+def Direc_find(a):
+	t=open(a[3],"r")
+	for i in t:	
+		x=repr(i).replace(r'\n',"")
+		req=requests.get(a[2]+"/"+x)
+		if req.status_code<300:
+			print(green+req.status_code+NC+" "+a[2]+"/"+x)
+		else:
+			print(RED+req.status_code+NC+" "+a[2]+"/"+x)
+
