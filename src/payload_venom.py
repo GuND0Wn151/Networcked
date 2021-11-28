de5e5d1dd9c514c42736e822203b2e682ce1e10a
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 14:56:00 +0530

    Update payload_venom.py

--- src/payload_venom.py
+++ src/payload_venom.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import PIPE, run
+import time
 def command(cmd):
 	return run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
@@ -37,6 +38,24 @@ def payloads_msf(a):
 	a=a.split()
 	if "netw" in a:
 		if "--payload" in a:
+			RED='\033[0;31m'
+			NC='\033[0m'
+			green='\033[0;32m'
+			cyan='\033[0;36m'
+			orange='\033[0;33m'
+			purple='\033[0;35m'
+			print()
+			os.system("echo '\033[0;31m [Starting] \033[0m Loading The Modules...'")
+			print()
+			time.sleep(1)
+			os.system("echo '\033[0;31m [Initializing] \033[0m Starting The Payload...'")
+			time.sleep(1)
+			print()
+			os.system("echo '\033[0;31m [Retrieving  the Data] \033[0m  Almost there.....'")
+			print()
+			time.sleep(1)
+			os.system("echo '\033[0;32m [Got The Info !!] \033'")
+			print()
 			try:
 				data=payloads[a[2]]
 				
