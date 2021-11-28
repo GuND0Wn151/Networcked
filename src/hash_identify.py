75c4c87fab82a460bafe7ee20d9ff50252dfcc08
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 14:38:36 +0530

    Update hash_identify.py

--- src/hash_identify.py
+++ src/hash_identify.py
@@ -5,10 +5,11 @@ def command(cmd):
 	return run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
 def hash_iden(a):
-
+	a=a.split()
 	if "--hash" in a:
 		print("The Possible Hashes Can be:- ")
 		print("\n---------------------------------------\n")
+		#print(a[2])
 		os.system("haiti "+a[2])
 		print("\n---------------------------------------\n")
 		#print(x)
