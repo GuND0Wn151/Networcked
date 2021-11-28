d47af26b25869287e83dacfc76f752e6741e4ebf
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 12:04:17 -0500

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("sudo apt-get install nmap")
+os.system("sudo apt-get install rubygems")
+os.system("sudo gem install haiti-hash")
+os.system("sudo apt-get install python3")
+os.sytem("pip install webbrowser")
+os.sytem("pip install BeautifulSoup")
+os.sytem("pip install requests")
+os.sytem("pip install pyautogui")
