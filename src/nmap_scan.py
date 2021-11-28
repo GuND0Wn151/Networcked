1e7bf5c596d2e6a8542efe4af9699ca7ee19a92c
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 14:55:40 +0530

    Update nmap_scan.py

--- src/nmap_scan.py
+++ src/nmap_scan.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import PIPE, run
+import time
 def command(cmd):
 	return run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
@@ -15,6 +16,24 @@ def print_ports(data):
 
 def nmap_scan_extra(a,ip):
 	if "--extra" in a:
+		RED='\033[0;31m'
+		NC='\033[0m'
+		green='\033[0;32m'
+		cyan='\033[0;36m'
+		orange='\033[0;33m'
+		purple='\033[0;35m'
+		print()
+		os.system("echo '\033[0;31m [Starting] \033[0m Loading The Modules...'")
+		print()
+		time.sleep(1)
+		os.system("echo '\033[0;31m [Initializing] \033[0m Starting The Scanning...'")
+		time.sleep(1)
+		print()
+		os.system("echo '\033[0;31m [Retrieving  the Data] \033[0m  Almost there.....'")
+		print()
+		time.sleep(1)
+		os.system("echo '\033[0;32m [Got The Info !!] \033'")
+		print()
 		params=""
 		for i in a:
 			if i[:2]=="--":
@@ -32,6 +51,24 @@ def nmap_scan(a):
 	if a[0]=="netw":
 		try:
 			if "--scan" or "--s" in a:
+				RED='\033[0;31m'
+				NC='\033[0m'
+				green='\033[0;32m'
+				cyan='\033[0;36m'
+				orange='\033[0;33m'
+				purple='\033[0;35m'
+				print()
+				os.system("echo '\033[0;31m [Starting] \033[0m Loading The Modules...'")
+				print()
+				time.sleep(1)
+				os.system("echo '\033[0;31m [Initializing] \033[0m Starting The Scanning...'")
+				time.sleep(1)
+				print()
+				os.system("echo '\033[0;31m [Retrieving  the Data] \033[0m  Almost there.....'")
+				print()
+				time.sleep(1)
+				os.system("echo '\033[0;32m [Got The Info !!] \033'")
+				print()
 				ip=""
 				for i in a:
 					if i.count('.')>2:
