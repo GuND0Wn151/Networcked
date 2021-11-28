7234628538c1d20891e58399f59ad360ed87f05e
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 14:53:39 +0530

    Update directory_finder.py

--- src/directory_finder.py
+++ src/directory_finder.py
@@ -2,17 +2,31 @@ import os
 import time
 import requests
 
-RED='\033[0;31m'
-NC='\033[0m'
-green='\033[0;32m'
-cyan='\033[0;36m'
-orange='\033[0;33m'
-purple='\033[0;35m'
 
 def Direc_find(a):
+	RED='\033[0;31m'
+	NC='\033[0m'
+	green='\033[0;32m'
+	cyan='\033[0;36m'
+	orange='\033[0;33m'
+	purple='\033[0;35m'
+
 	a=a.split()
 	if(a[0]=="netw"):
 		if a[1]=="--direc":
+
+			print()
+			os.system("echo '\033[0;31m [Starting] \033[0m Loading The Modules...'")
+			print()
+			time.sleep(1)
+			os.system("echo '\033[0;31m [Initializing] \033[0m Starting The Opeing the file...'")
+			time.sleep(1)
+			print()
+			os.system("echo '\033[0;31m [Retrieving  the Data] \033[0m  Almost there.....'")
+			print()
+			time.sleep(1)
+			os.system("echo '\033[0;32m [Got The Info !!] \033'")
+			print()
 			t=open(a[3],"r")
 			for i in t:	
 				x=repr(i).replace(r'\n',"")
