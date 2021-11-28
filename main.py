84d1192857ad55be6e37859393fe7da025b562e2
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-11-28 02:44:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+from src.logo import logo_print
+from src.data import *
+from src.apache_server import start_server
+from src.directory_finder import Direc_find
+from src.nmap_scan import *
+from src.owasp_top import get_top_10
+from src.searchsploit import searchsploit
+from src.hash_identify import hash_iden
+from src.cve_details import CVE_details
+
+
+RED='\033[0;31m'
+nc='\033[0m'
+green='\033[0;32m'
+cyan='\033[0;36m'
+orange='\033[0;33m'
+purple='\033[0;35m'
+import os
+os.system("clear")
+logo_print()
+
+print(RED+"\t\n-----List of Services-------"+nc)
+print(cyan+"\t\n1. Python/Apache Server")
+print("\t\n2. Port Scanning Using Nmap ")
+print("\t\n3. Documentations and Information")
+print("\t\n4. Scripts and Payloads")
+print("\t\n5. Hashes Identifer")
+print("\t\n6. Directory and CVE Details"+nc)
+print(RED+"\t\n------------------------------"+nc+"\n")
+
+a=input("Choose The Category:- ")
+if a=="1":
+	apache_server()
+	directory_finder()
+	print()
+	data=input("Enter the Command:- ")
+
+	start_server(data)
+
+	Direc_find(data)
+	
+
+elif a=="2":
+	nmap_scan()
+	print()
+	data=input("Enter the Command:- ")
+	if "--extra" in data:
+		ip=input("Enter the IP:- ")
+		nmap_scan_extra(data,ip)
+	else:
+		nmap_scan()
+
+elif a=="3":
+	Owasp_top()
+	print()
+	data=input("Enter the Command:- ")
+	get_top_10()
+
+
+elif a=="4":
+	payload_venom()
+	searchsploit()
+	print()
+	data=input("Enter the Command:- ")
+	payloads_msf(data)
+	searchsploit(data)
+
+elif a=="5":
+	hash_identify()
+	print()
+	data=input("Enter the Command:- ")
+	hash_iden()
+
+elif a=="6":
+	cve_detalis()
+	print()
+	data=input("Enter the Command:- ")
+	CVE_details(data)
